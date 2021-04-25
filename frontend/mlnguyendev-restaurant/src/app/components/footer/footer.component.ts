b8182dec145dc91d1819d3b9ea5b5dfacdbd6e07
mlnguyendeveloper <mlnguyendev@gmail.com>
2021-04-24 23:42:55 -0400

    Added footer

---
+++ frontend/mlnguyendev-restaurant/src/app/components/footer/footer.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+  constructor() {
+   }
+
+  ngOnInit(): void {
+
+  }
+
+}
