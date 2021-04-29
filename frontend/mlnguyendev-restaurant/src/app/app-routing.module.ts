d843d00972a0071b3f93cf846662a54c81c2c8a6
mlnguyendeveloper <mlnguyendev@gmail.com>
2021-04-28 21:32:40 -0400

    Added a notfoundComponent for to redirect

--- frontend/mlnguyendev-restaurant/src/app/app-routing.module.ts
+++ frontend/mlnguyendev-restaurant/src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { AboutComponent } from './components/content/about/about.component';
 import { HomeComponent } from './components/content/home/home.component';
 import { LocationsComponent } from './components/content/locations/locations.component';
 import { MenuComponent } from './components/content/menu/menu.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'locations', component: LocationsComponent },
+  { path: 'locations/:name', component: LocationsComponent },
   { path: 'menu', component: MenuComponent },
-  { path: '**', component: HomeComponent } // TODO: make a 404 not found page
+  { path: '**', component: NotfoundComponent } 
 ];
 
 const routerOptions: ExtraOptions = {
