d9d8a405168dd56ff64eae314da2715a42d32d19
mlnguyendeveloper <mlnguyendev@gmail.com>
2021-04-29 15:03:46 -0400

    Added backend starter files

---
+++ backend/mlnguyen-restaurant/src/main/java/com/mlnguyen/mlnguyenrestaurant/MlnguyenRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.mlnguyen.mlnguyenrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlnguyenRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlnguyenRestaurantApplication.class, args);
+	}
+
+}
---
+++ backend/mlnguyen-restaurant/src/test/java/com/mlnguyen/mlnguyenrestaurant/MlnguyenRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlnguyen.mlnguyenrestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlnguyenRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
