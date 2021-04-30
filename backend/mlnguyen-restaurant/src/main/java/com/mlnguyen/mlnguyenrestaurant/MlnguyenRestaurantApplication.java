6983268f9de00ed2f727f3736b13016c86438bcf
mlnguyendeveloper <mlnguyendev@gmail.com>
2021-04-29 21:44:10 -0400

    Added backend configs

--- backend/mlnguyen-restaurant/src/main/java/com/mlnguyen/mlnguyenrestaurant/MlnguyenRestaurantApplication.java
+++ backend/mlnguyen-restaurant/src/main/java/com/mlnguyen/mlnguyenrestaurant/MlnguyenRestaurantApplication.java
@@ -2,8 +2,12 @@ package com.mlnguyen.mlnguyenrestaurant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@ComponentScan
 public class MlnguyenRestaurantApplication {
 
 	public static void main(String[] args) {
