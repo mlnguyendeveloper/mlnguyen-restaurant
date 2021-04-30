a25c84b723d01623cbfe7a38ee3e0fac88f88909
mlnguyendeveloper <mlnguyendev@gmail.com>
2021-04-30 00:03:04 -0400

    Updated menuComponent to use MenuService

--- frontend/mlnguyendev-restaurant/src/app/components/content/menu/menu.component.ts
+++ frontend/mlnguyendev-restaurant/src/app/components/content/menu/menu.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuCategory } from 'src/app/common/menu-category';
+import { MenuItem } from 'src/app/common/menu-item';
+import { MenuService } from 'src/app/services/menu.service';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  //menuItems!: MenuItem[];
+  menuCategories!: MenuCategory[];
+
+  constructor(private menuService: MenuService) { }
 
   ngOnInit(): void {
+    this.listMenuCategories();
+    console.log(this.menuCategories);
   }
 
+  listMenuCategories(){
+    this.menuService.getMenuCategoriesList().subscribe(
+      data => {
+        this.menuCategories = data;
+        console.log('Menu Categories=' + JSON.stringify(data));
+      }
+    )
+  }
 }
