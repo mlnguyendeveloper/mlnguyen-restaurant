3e3f890784cd3fd3936167683ee31c38d21f96ab
mlnguyendeveloper <mlnguyendev@gmail.com>
2021-04-30 00:02:28 -0400

    Added menuItem class

---
+++ frontend/mlnguyendev-restaurant/src/app/common/menu-item.ts
@@ -0,0 +1,11 @@
+export class MenuItem {
+
+    id!: number;
+    name!: string;
+    description!: string;
+    price!: string;
+    imageUrl?: string;
+    active!: boolean;
+    dateCreated!: Date;
+    dateUpdated?: Date;
+}
