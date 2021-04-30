4ada1d2171a11ab42f5ccb9602727d82e650cd39
mlnguyendeveloper <mlnguyendev@gmail.com>
2021-04-30 00:02:19 -0400

    Added MenuCategory class

---
+++ frontend/mlnguyendev-restaurant/src/app/common/menu-category.spec.ts
@@ -0,0 +1,7 @@
+import { MenuCategory } from './menu-category';
+
+describe('MenuCategory', () => {
+  it('should create an instance', () => {
+    expect(new MenuCategory()).toBeTruthy();
+  });
+});
---
+++ frontend/mlnguyendev-restaurant/src/app/common/menu-category.ts
@@ -0,0 +1,5 @@
+export class MenuCategory {
+    id!: number;
+    categoryName!: string;
+    description?: string;
+}
