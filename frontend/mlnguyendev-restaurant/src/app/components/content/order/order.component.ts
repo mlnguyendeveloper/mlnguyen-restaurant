3a65e85789f240371f6b8081d8e02784e68fe00c
mlnguyendeveloper <mlnguyendev@gmail.com>
2021-05-04 22:16:22 -0400

    Added serve to component to load menu items on page

--- frontend/mlnguyendev-restaurant/src/app/components/content/order/order.component.ts
+++ frontend/mlnguyendev-restaurant/src/app/components/content/order/order.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuCategory } from 'src/app/common/menu-category';
+import { MenuService } from 'src/app/services/menu.service';
+import { Menu } from 'src/app/constants/menu';
 
 @Component({
   selector: 'app-order',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
-  constructor() { }
+    //Hardcoded categories to ensure order
+    menuCategoryNames: string[];
+    menuCategoryColorCodes: string[]; 
+    
+    menuCategories: MenuCategory[] = [];
 
-  ngOnInit(): void {
+  constructor(private menuService: MenuService) { 
+    this.menuCategoryNames = Menu.menuCategoryNames;
+    this.menuCategoryColorCodes = Menu.menuCategoryColorCodes;
   }
 
+  ngOnInit(): void {
+    this.populateMenuCategories();
+  }
+
+  populateMenuCategories() {
+    this.menuService.getMenuCategoriesList().subscribe(
+      data => {
+        console.log(data);
+        this.menuCategories = data;
+      }
+    )
+  }
 }
